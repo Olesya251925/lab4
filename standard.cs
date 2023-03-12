a222eee024547d706194b04abfcac1efb4e8fe98
Olesya251925 <118814113+Olesya251925@users.noreply.github.com>
2023-03-12 23:20:40 +0700

    Update standard.cs

--- standard.cs
+++ standard.cs
@@ -1,7 +1,3 @@
-/*****************************
- *   Лабораторная работа      *
- *             № 4            *
- ******************************/
  using System;
  using System.Text;
  using System.Runtime.Serialization.Formatters.Binary;
